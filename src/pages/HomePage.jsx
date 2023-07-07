0bd566cb669d09606d593a6e60fb836fda5bbafe
Zehra Celik <escev123@gmail.com>
2023-07-08 01:12:29 +0300

    last commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Main from '../components/Main'
+import Card from '../components/card/Card'
+import Work from '../components/card/Work'
+
+const HomePage = () => {
+  return (
+    <div >
+        <Main />
+        <Card />
+        <Work />
+    </div>
+  )
+}
+
+export default HomePage
