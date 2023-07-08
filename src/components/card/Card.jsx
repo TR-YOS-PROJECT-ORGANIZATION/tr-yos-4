98fa409b92877e93937a3c05a7af7861b0962c91
MeralSavas <meralsavas@gmail.com>
2023-07-08 13:10:58 +0100

    changed card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card1 = () => {
   return (
-    <div className="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="grid grid-flow-row gap-6 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       <>
         {/* component */}
         <div className="relative mx-auto w-full max-w-sm pt-6 ml-6">
