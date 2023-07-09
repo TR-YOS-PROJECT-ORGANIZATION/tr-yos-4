bfe9f54dd1acde963b88aabe6d6cd3fee8d87c6b
Sevda <morkocsevdam@gmail.com>
2023-07-09 18:18:53 +0300

    universty card upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import "./App.css";
 import Card1 from "./components/card/Card";
 import Departments from "./pages/Departments";
 import React from 'react';
-import UniversityCard from './components/card/UniversityCard';
 import UniversitiesPage from './pages/UniversitiesPage';
+import UniversityCard from './components/card/UniversityCard';
+
 
 const App = () => {
   return (
     <div>
       <h1>My App</h1>
-      <Departments/>
-      <UniversityCard />
+      <Departments/> 
       <UniversitiesPage />
+      <UniversityCard />
+     
     </div>
   );
 }
--- src/components/card/UniversityCard.jsx
+++ src/components/card/UniversityCard.jsx
@@ -2,16 +2,133 @@ import React from 'react';
 
 const UniversityCard = () => {
   return (
-    <div className>
-      
-      <div className>
-        
-        <div className>
-          
+
+    <>
+
+      <div className="max-w-5xl flex flex-col mx-auto px-4 sm:px-6 lg:px-8justify-center items-center sm:flex-row sm:justify-start border-2 border-gray-300 rounded-md mx-25 mt-2 mb-6">
+
+        <img src="https://aku.edu.tr/wp-content/themes/akuyenitema/img/aku.png" alt="" />
+
+        <div>
+          <p className="font-bold text-xl mb-2">AFYON KOCATEPE UNİVERSTY</p>
+          <p className="text-xs mb-2">
+            Afyon kocatepe Üniversitesi Rektörlüğü ANS Kampüsü Gazlıgöl Yolu Üzeri AFYONKARAHİSAR
+          </p>
+          <svg className="h-4 w-4 text-gray-400 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+          </svg>
+          <p className="ml-1 text-sm text-gray-500 inline-block">Afyonkarahisar</p>
+
+
+          <div className="my-4">
+            <div className="flex">
+              <p className="text-xs mr-4 mb-2 p-2 font-medium border-none rounded-md bg-orange-100 text-orange-500">1 Faculties</p>
+              <p className="text-xs mr-2 mb-2 p-2 font-medium border-none rounded-md bg-green-100 text-green-500">2 Departments</p>
+              <button className="text-xs mr-2 mb-2 p-2 font-medium border-none border-orange-500 rounded-md bg-orange-100 text-orange-500 hover:bg-orange-500 hover:text-white">P (352)-444-9977</button>
+              <button className='text-xs mr-4 mb-2 p-2 font-medium border-none rounded-md bg-orange-100 text-orange-500'>
+                <a
+                  href="https://aku.edu.tr/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:text-blue-700"
+                >
+                  Visit Website
+                </a></button>
+
+            </div>
+
+
+          </div>
+        </div>
+
+
+      </div>
+
+
+      <div className="max-w-5xl flex flex-col mx-auto px-4 sm:px-6 lg:px-8justify-center items-center sm:flex-row sm:justify-start border-2 border-gray-300 rounded-md mx-25 mt-2 mb-6">
+
+
+        <img src="https://aku.edu.tr/wp-content/themes/akuyenitema/img/aku.png" alt="" />
+
+
+
+        <div>
+          <p className="font-bold text-xl mb-2">AFYON KOCATEPE UNİVERSTY</p>
+          <p className="text-xs mb-2">
+            Afyon kocatepe Üniversitesi Rektörlüğü ANS Kampüsü Gazlıgöl Yolu Üzeri AFYONKARAHİSAR
+          </p>
+          <svg className="h-4 w-4 text-gray-400 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+          </svg>
+          <p className="ml-1 text-sm text-gray-500 inline-block">Afyonkarahisar</p>
+
+
+
+          <div className="my-4">
+            <div className="flex">
+              <p className="text-xs mr-4 mb-2 p-2 font-medium border-none rounded-md bg-orange-100 text-orange-500">1 Faculties</p>
+              <p className="text-xs mr-2 mb-2 p-2 font-medium border-none rounded-md bg-green-100 text-green-500">2 Departments</p>
+              <button className="text-xs mr-2 mb-2 p-2 font-medium border-none border-orange-500 rounded-md bg-orange-100 text-orange-500 hover:bg-orange-500 hover:text-white">P (352)-444-9977</button>
+              <button className='text-xs mr-4 mb-2 p-2 font-medium border-none rounded-md bg-orange-100 text-orange-500'>
+                <a
+                  href="https://aku.edu.tr/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:text-blue-700"
+                >
+                  Visit Website
+                </a></button>
+
+            </div>
+
+
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
 
+      <div className="max-w-5xl flex flex-col mx-auto px-4 sm:px-6 lg:px-8justify-center items-center sm:flex-row sm:justify-start border-2 border-gray-300 rounded-md mx-25 mt-2 mb-6">
+
+
+        <img src="https://aku.edu.tr/wp-content/themes/akuyenitema/img/aku.png" alt="" />
+
+
+
+        <div>
+          <p className="font-bold text-xl mb-2">AFYON KOCATEPE UNİVERSTY</p>
+          <p className="text-xs mb-2">
+            Afyon kocatepe Üniversitesi Rektörlüğü ANS Kampüsü Gazlıgöl Yolu Üzeri AFYONKARAHİSAR
+          </p>
+          <svg className="h-4 w-4 text-gray-400 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+          </svg>
+          <p className="ml-1 text-sm text-gray-500 inline-block">Afyonkarahisar</p>
+
+
+
+          <div className="my-4">
+            <div className="flex">
+              <p className="text-xs mr-4 mb-2 p-2 font-medium border-none rounded-md bg-orange-100 text-orange-500">1 Faculties</p>
+              <p className="text-xs mr-2 mb-2 p-2 font-medium border-none rounded-md bg-green-100 text-green-500">2 Departments</p>
+              <button className="text-xs mr-2 mb-2 p-2 font-medium border-none border-orange-500 rounded-md bg-orange-100 text-orange-500 hover:bg-orange-500 hover:text-white">P (352)-444-9977</button>
+              <button className='text-xs mr-4 mb-2 p-2 font-medium border-none rounded-md bg-orange-100 text-orange-500'>
+                <a
+                  href="https://aku.edu.tr/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:text-blue-700"
+                >
+                  Visit Website
+                </a></button>
+
+            </div>
+
+
+          </div>
+        </div>
+
+
+      </div>
+    </>
+  );
+};
 export default UniversityCard;
