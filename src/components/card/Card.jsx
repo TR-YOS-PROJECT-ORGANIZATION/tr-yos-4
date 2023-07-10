d0fe90e7c78260da88dcbc314d6fa914bdc56ca1
MeralSavas <meralsavas@gmail.com>
2023-07-10 12:27:02 +0100

    fixed size icon

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -67,8 +67,8 @@ const Card1 = () => {
                 <div className="mt-2 grid grid-cols-2 grid-rows-1 gap-4 border-b border-t border-gray-200 pb-3 pt-3">
                   <p className="flex items-center text-gray-800 xl:flex-row xl:items-center">
                     <svg
-                      width="20px"
-                      height="20px"
+                      width="25px"
+                      height="25px"
                       viewBox="0 0 24 24"
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
