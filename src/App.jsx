6c7f665e986ce630c19dac9d2c61b9de30a2c632
MEHMETTOKGOZ <42mt06@gmail.com>
2023-07-10 23:16:08 +0300

    responsive for sm-md screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Card1 from "./components/card/Card";
+import Departments from "./pages/Departments";
 import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <>
       <HomePage />
+      
       
     </>
   );
