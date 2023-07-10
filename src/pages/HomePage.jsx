5540ddc170dae8a76614e6df414a3950e8c3ab58
Zehra Celik <escev123@gmail.com>
2023-07-10 17:08:44 +0300

    App Router added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Main from '../components/Main'
 import Card from '../components/card/Card'
-import Navbar from '../components/Navbar'
 
 const HomePage = () => {
   return (
     <div >
-        <Navbar />
         <Main />
         <Card />
     </div>
