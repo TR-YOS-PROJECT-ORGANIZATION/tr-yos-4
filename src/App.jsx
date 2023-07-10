077062698aeb51898a8e95891e6e3126a21884fa
Sevda <morkocsevdam@gmail.com>
2023-07-10 23:19:21 +0300

    app js eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Card1 from "./components/card/Card";
 import HomePage from './pages/HomePage'
+import UniversitiesPage from "./pages/UniversitiesPage"; 
+import UniversityCard from "./components/card/UniversityCard";
 
 function App() {
   return (
     <>
       <HomePage />
+      <UniversitiesPage/>
+      <UniversityCard/>
       
     </>
   );
