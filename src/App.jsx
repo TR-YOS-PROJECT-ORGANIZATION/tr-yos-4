6b7ace0812154e4bd2d16b48eb9ca7a03c7b3806
Sevda <morkocsevdam@gmail.com>
2023-07-10 23:28:53 +0300

    appjs silindi

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
-import Card1 from "./components/card/Card";
-import HomePage from './pages/HomePage'
+
+
 import UniversitiesPage from "./pages/UniversitiesPage"; 
-import UniversityCard from "./components/card/UniversityCard";
+
 
 function App() {
   return (
     <>
-      <HomePage />
+      
       <UniversitiesPage/>
-      <UniversityCard/>
+      
       
     </>
   );
