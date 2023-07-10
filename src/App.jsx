a478f2a5477136d6ab0a82a32a889827f9e0708e
gulfeyzaciftci <gulfeyza333@gmail.com>
2023-07-10 15:12:27 +0200

    footer added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Card1 from "./components/card/Card";
 import Departments from "./pages/Departments";
-import HomePage from './pages/HomePage'
+import HomePage from "./pages/HomePage";
+import Footer from "../src/footer/Footer";
 
 function App() {
   return (
     <>
       <HomePage />
+      <Footer />
     </>
   );
 }
