af8dc46ef39053414442f018ae2758a935364cac
Sevda <morkocsevdam@gmail.com>
2023-07-10 14:48:15 +0300

    phone icon eklndi

--- src/components/card/UniversityCard.jsx
+++ src/components/card/UniversityCard.jsx
@@ -2,131 +2,209 @@ import React from 'react';
 
 const UniversityCard = () => {
   return (
-   
+
     <>
 
-      <div className="max-w-5xl flex flex-col mx-auto px-4 sm:px-6 lg:px-8justify-center items-center sm:flex-row sm:justify-start border-2 border-gray-300 rounded-md mx-25 mt-2 mb-6">
-
-        
-          <img  src="https://aku.edu.tr/wp-content/themes/akuyenitema/img/aku.png" alt="" />
-       
+      <div className="max-w-5xl flex flex-col mx-auto px-4 sm:px-6 lg:px-8justify-center items-center sm:flex-row sm:justify-start border-2 border-gray-300 rounded-md mx-25 mt-4 mb-2">
 
 
-          <div>
-            <p className="font-bold text-xl mb-2">AFYON KOCATEPE UNİVERSTY</p>
-            <p className="text-xs mb-2">
+        <img src="https://aku.edu.tr/wp-content/themes/akuyenitema/img/aku.png" alt="" />
+
+
+
+        <div>
+          <p className="font-bold text-xl mb-2">AFYON KOCATEPE UNİVERSTY</p>
+          <p className="text-xs mb-2">
             Afyon kocatepe Üniversitesi Rektörlüğü ANS Kampüsü Gazlıgöl Yolu Üzeri AFYONKARAHİSAR
-            </p>
-            <svg className="h-4 w-4 text-gray-400 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-        </svg>
-        <p className="ml-1 text-sm text-gray-500 inline-block">Afyonkarahisar</p>
+          </p>
+          <svg className="h-4 w-4 text-gray-400 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+          </svg>
+          <p className="ml-1 text-sm text-gray-500 inline-block">Afyonkarahisar</p>
 
-            
 
-        <div className="my-4">
-          <div className="flex">
-            <p className="text-xs mr-4 mb-2 p-2 font-medium border-none rounded-md bg-orange-100 text-orange-500">1 Faculties</p>
-            <p className="text-xs mr-2 mb-2 p-2 font-medium border-none rounded-md bg-green-100 text-green-500">2 Departments</p>
-            <button className="text-xs mr-2 mb-2 p-2 font-medium border-none border-orange-500 rounded-md bg-orange-100 text-orange-500 hover:bg-orange-500 hover:text-white">P (352)-444-9977</button>
+
+          <div className="my-4">
+            <div className="flex">
+              <p className="text-xs mr-4 mb-2 p-2 font-medium border-none rounded-md bg-orange-100 text-orange-500">1 Faculties</p>
+              <p className="text-xs mr-2 mb-2 p-2 font-medium border-none rounded-md bg-green-100 text-green-500">2 Departments</p>
+              <button className=" flex text-xs mr-2 mb-2 p-2 font-medium border-none border-orange-500 rounded-md bg-orange-100 text-orange-500 hover:bg-orange-500 hover:text-white">
+
+                <div className="pt-1 m-0.5">
+                  <svg
+                    className="fill-green"
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="0.9em"
+                    viewBox="0 0 512 512"
+                  >
+                    <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+                  </svg>
+                </div>
+
+
+
+                <div>(352)-444-9977</div>
+
+
+              </button>
+
               <button className='text-xs mr-4 mb-2 p-2 font-medium border-none rounded-md bg-orange-100 text-orange-500'>
-            <a
-                href="https://aku.edu.tr/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 hover:text-blue-700"
-              >
-                Visit Website
-              </a></button>
+                <a
+                  href="https://aku.edu.tr/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:text-blue-700"
+                >
+                  Visit Website
+                </a></button>
 
-          </div>
-           
-            
+            </div>
+
+
           </div>
         </div>
 
 
       </div>
 
-      
-      <div className="max-w-5xl flex flex-col mx-auto px-4 sm:px-6 lg:px-8justify-center items-center sm:flex-row sm:justify-start border-2 border-gray-300 rounded-md mx-25 mt-2 mb-6">
 
-        
-          <img  src="https://aku.edu.tr/wp-content/themes/akuyenitema/img/aku.png" alt="" />
-       
+      <div className="max-w-5xl flex flex-col mx-auto px-4 sm:px-6 lg:px-8justify-center items-center sm:flex-row sm:justify-start border-2 border-gray-300 rounded-md mx-25 mt-2 mb-2">
 
 
-          <div>
-            <p className="font-bold text-xl mb-2">AFYON KOCATEPE UNİVERSTY</p>
-            <p className="text-xs mb-2">
+        <img src="https://aku.edu.tr/wp-content/themes/akuyenitema/img/aku.png" alt="" />
+
+
+
+        <div>
+          <p className="font-bold text-xl mb-2">AFYON KOCATEPE UNİVERSTY</p>
+          <p className="text-xs mb-2">
             Afyon kocatepe Üniversitesi Rektörlüğü ANS Kampüsü Gazlıgöl Yolu Üzeri AFYONKARAHİSAR
-            </p>
-            <svg className="h-4 w-4 text-gray-400 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-        </svg>
-        <p className="ml-1 text-sm text-gray-500 inline-block">Afyonkarahisar</p>
+          </p>
+          <svg className="h-4 w-4 text-gray-400 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+          </svg>
+          <p className="ml-1 text-sm text-gray-500 inline-block">Afyonkarahisar</p>
 
-            
 
-        <div className="my-4">
-          <div className="flex">
-            <p className="text-xs mr-4 mb-2 p-2 font-medium border-none rounded-md bg-orange-100 text-orange-500">1 Faculties</p>
-            <p className="text-xs mr-2 mb-2 p-2 font-medium border-none rounded-md bg-green-100 text-green-500">2 Departments</p>
-            <button className="text-xs mr-2 mb-2 p-2 font-medium border-none border-orange-500 rounded-md bg-orange-100 text-orange-500 hover:bg-orange-500 hover:text-white">P (352)-444-9977</button>
+
+          <div className="my-4">
+            <div className="flex">
+              <p className="text-xs mr-4 mb-2 p-2 font-medium border-none rounded-md bg-orange-100 text-orange-500">1 Faculties</p>
+              <p className="text-xs mr-2 mb-2 p-2 font-medium border-none rounded-md bg-green-100 text-green-500">2 Departments</p>
+
+
+
+              <button className=" flex text-xs mr-2 mb-2 p-2 font-medium border-none border-orange-500 rounded-md bg-orange-100 text-orange-500 hover:bg-orange-500 hover:text-white">
+
+                <div className="pt-1 m-0.5">
+                  <svg
+                    className="fill-green"
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="0.9em"
+                    viewBox="0 0 512 512"
+                  >
+                    <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+                  </svg>
+                </div>
+
+
+
+                <div>(352)-444-9977</div>
+
+
+              </button>
+
+
+
+
+
+
+
               <button className='text-xs mr-4 mb-2 p-2 font-medium border-none rounded-md bg-orange-100 text-orange-500'>
-            <a
-                href="https://aku.edu.tr/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 hover:text-blue-700"
-              >
-                Visit Website
-              </a></button>
+                <a
+                  href="https://aku.edu.tr/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:text-blue-700"
+                >
+                  Visit Website
+                </a></button>
 
-          </div>
-           
-            
+            </div>
+
+
           </div>
         </div>
       </div>
-      
+
       <div className="max-w-5xl flex flex-col mx-auto px-4 sm:px-6 lg:px-8justify-center items-center sm:flex-row sm:justify-start border-2 border-gray-300 rounded-md mx-25 mt-2 mb-6">
 
-        
-          <img  src="https://aku.edu.tr/wp-content/themes/akuyenitema/img/aku.png" alt="" />
-       
+
+        <img src="https://aku.edu.tr/wp-content/themes/akuyenitema/img/aku.png" alt="" />
 
 
-          <div>
-            <p className="font-bold text-xl mb-2">AFYON KOCATEPE UNİVERSTY</p>
-            <p className="text-xs mb-2">
+
+        <div>
+          <p className="font-bold text-xl mb-2">AFYON KOCATEPE UNİVERSTY</p>
+          <p className="text-xs mb-2">
             Afyon kocatepe Üniversitesi Rektörlüğü ANS Kampüsü Gazlıgöl Yolu Üzeri AFYONKARAHİSAR
-            </p>
-            <svg className="h-4 w-4 text-gray-400 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-        </svg>
-        <p className="ml-1 text-sm text-gray-500 inline-block">Afyonkarahisar</p>
+          </p>
+          <svg className="h-4 w-4 text-gray-400 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+          </svg>
+          <p className="ml-1 text-sm text-gray-500 inline-block">Afyonkarahisar</p>
 
-            
 
-        <div className="my-4">
-          <div className="flex">
-            <p className="text-xs mr-4 mb-2 p-2 font-medium border-none rounded-md bg-orange-100 text-orange-500">1 Faculties</p>
-            <p className="text-xs mr-2 mb-2 p-2 font-medium border-none rounded-md bg-green-100 text-green-500">2 Departments</p>
-            <button className="text-xs mr-2 mb-2 p-2 font-medium border-none border-orange-500 rounded-md bg-orange-100 text-orange-500 hover:bg-orange-500 hover:text-white">P (352)-444-9977</button>
+
+          <div className="my-4">
+            <div className="flex">
+              <p className="text-xs mr-4 mb-2 p-2 font-medium border-none rounded-md bg-orange-100 text-orange-500">1 Faculties</p>
+              <p className="text-xs mr-2 mb-2 p-2 font-medium border-none rounded-md bg-green-100 text-green-500">2 Departments</p>
+
+
+              <button className=" flex text-xs mr-2 mb-2 p-2 font-medium border-none border-orange-500 rounded-md bg-orange-100 text-orange-500 hover:bg-orange-500 hover:text-white">
+
+                <div className="pt-1 m-0.5">
+                  <svg
+                    className="fill-green"
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="0.9em"
+                    viewBox="0 0 512 512"
+                  >
+                    <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+                  </svg>
+                </div>
+
+
+
+                <div>(352)-444-9977</div>
+
+
+              </button>
+
+
+
+
+
+
+
+
+
+
+
               <button className='text-xs mr-4 mb-2 p-2 font-medium border-none rounded-md bg-orange-100 text-orange-500'>
-            <a
-                href="https://aku.edu.tr/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 hover:text-blue-700"
-              >
-                Visit Website
-              </a></button>
+                <a
+                  href="https://aku.edu.tr/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:text-blue-700"
+                >
+                  Visit Website
+                </a></button>
 
-          </div>
-           
-            
+            </div>
+
+
           </div>
         </div>
 
