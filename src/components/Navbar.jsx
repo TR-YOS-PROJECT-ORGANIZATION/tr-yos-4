589bc31da6eaa165addb8f27cc2d98abcf89e8c2
Zehra Celik <escev123@gmail.com>
2023-07-12 18:23:26 +0300

    navbar-last-changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Hamburger from "hamburger-react";
 import { useState } from "react";
+import SignInModal from "./SignInModal";
+import SignUpModal from "./SignUpModal";
 
 const Navbar = () => {
   const [isOpen, setOpen] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [openUpModal, setOpenUpModal] = useState(false);
+
 
   const currentUser = false;
 
@@ -12,8 +17,20 @@ const Navbar = () => {
 
   return (
     <div  >
+
+<SignInModal
+  open={openModal}
+  setOpen={setOpenModal}
+  setOpenUpModal={setOpenUpModal}
+/>
+<SignUpModal
+  open={openUpModal}
+  setOpen={setOpenUpModal}
+  setOpenUpModal={setOpenModal}
+/>
+
       <div className="relative">
-      <nav className="rounded flex items-center md:justify-between sm:justify-around py-5 font-bold text-md fixed right-0 left-0 top-0 z-10 bg-white-500 border-t-[2rem] border-green-dark">
+      <nav className="rounded flex items-center md:justify-between sm:justify-around py-5 font-bold text-md fixed right-0 left-0 top-0 z-10 bg-white-500 border-t-[2rem] border-cyan-900">
         <div className="lg:flex lg:flex-row items-center sm:flex-col sm:flex ">
           <button className="mx-8 sm:mx-1 lg:invisible md:visible sm:visible relative">
             <Hamburger
@@ -73,7 +90,7 @@ const Navbar = () => {
 
 
         </div>
-        <div>
+        {/* <div>
         
           <button className="mx-3 bg-red-retro py-3 px-5 rounded text-white-cream hover:bg-red-warm hover:text-black">
             Sign In
@@ -81,7 +98,27 @@ const Navbar = () => {
           <button className="mx-3 bg-red-warm py-3 px-5 rounded text-white-cream hover:bg-red-retro hover:text-black">
             Sign Up
           </button>
-        </div>
+        </div> */}
+        {
+          //  navbar bölümü
+          <div className="w-full h-[82px]  flex items-center justify-end">
+            <div className="flex gap-4 h-[51px]  pr-[100px]">
+              <button
+                onClick={() => setOpenModal(!openModal)}
+                className="border rounded bg-green-dark hover:bg-red-warm text-white-cream px-4 py-2"
+              >
+                Sign In
+              </button>
+              <button
+                onClick={() => setOpenUpModal(!openUpModal)}
+                className="border rounded bg-green-dark hover:bg-red-warm	 text-white-cream px-4 py-2"
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+        }
+
       </nav>
       </div>
      
