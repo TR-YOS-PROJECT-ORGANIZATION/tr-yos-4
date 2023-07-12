21ebc68cf64abae791b795939cd3af4a61987fd5
Zehra Celik <escev123@gmail.com>
2023-07-13 02:12:56 +0300

    navbar-slider-responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,104 +4,98 @@ import Hamburger from "hamburger-react";
 import { useState } from "react";
 import SignInModal from "./SignInModal";
 import SignUpModal from "./SignUpModal";
-import langIcon from '../images/global.png'
+import langIcon from "../images/global.png";
 
 const Navbar = () => {
   const [isOpen, setOpen] = useState(false);
   const [openModal, setOpenModal] = useState(false);
   const [openUpModal, setOpenUpModal] = useState(false);
 
-
   const currentUser = false;
 
   console.log(isOpen);
 
   return (
-    <div  >
-
-<SignInModal
-  open={openModal}
-  setOpen={setOpenModal}
-  setOpenUpModal={setOpenUpModal}
-/>
-<SignUpModal
-  open={openUpModal}
-  setOpen={setOpenUpModal}
-  setOpenUpModal={setOpenModal}
-/>
+    <div>
+      <SignInModal
+        open={openModal}
+        setOpen={setOpenModal}
+        setOpenUpModal={setOpenUpModal}
+      />
+      <SignUpModal
+        open={openUpModal}
+        setOpen={setOpenUpModal}
+        setOpenUpModal={setOpenModal}
+      />
 
       <div className="relative">
-      <nav className="rounded flex items-center md:justify-between sm:justify-around py-5 font-bold text-md fixed right-0 left-0 top-0 z-10 bg-white-500 border-t-[2rem] border-green-dark">
-        <div className="lg:flex lg:flex-row items-center sm:flex-col sm:flex ">
-          <button className="mx-8 sm:mx-1 lg:invisible md:visible sm:visible relative">
-            <Hamburger
-              label="show menu"
-              toggled={isOpen}
-              toggle={setOpen}
-              direction="right"
-            />
-             {isOpen && (
-              
-                <ul className="md:flex-col absolute z-10 rounded">
-                  <li className="border border-green-dark px-3  py-2 bg-white-cream hover:bg-red-retro">
-                 
+        <nav className="rounded flex items-center md:justify-between sm:justify-around py-5 font-bold text-md fixed right-0 left-0 top-0 z-10 bg-white-500 border-t-[2rem] border-green-dark">
+          <div className="lg:flex lg:flex-row items-center sm:flex-col sm:flex ">
+            <button className="mx-8 sm:mx-1 lg:invisible md:visible sm:visible  relative">
+              <Hamburger
+                label="show menu"
+                toggled={isOpen}
+                toggle={setOpen}
+                direction="right"
+              />
+              {isOpen && (
+                <ul className="md:flex-col absolute z-10 rounded md:-left-36 sm:-left-10  max-[600px]:-left-10 ">
+                  <li className=" px-10  py-5 bg-white-500 hover:bg-red-retro ">
                     <Link to="/" className="mx-3">
-                      Home 
+                      Home
                     </Link>
                   </li>
-                  <li className="border  border-green-dark  px-3  py-2  bg-white-cream  hover:bg-red-retro">
+                  <li className="  px-10  py-5  bg-white-500  hover:bg-red-retro">
                     <Link to="/Universities" className="mx-3">
                       Universities
                     </Link>
                   </li>
-                  <li className="border  border-green-dark  px-3  py-2   bg-white-cream  hover:bg-red-retro">
+                  <li className="  px-10  py-5   bg-white-500  hover:bg-red-retro">
                     <Link to="/Departments" className="mx-3">
                       Departments
                     </Link>
                   </li>
-                  <li className="border  border-green-dark  px-3  py-2   bg-white-cream hover:bg-red-retro">                 
-                 <Link to="/About" className="mx-3">
-                   About
-                 </Link>
-               </li>
+                  <li className="  px-10  py-5   bg-white-500 hover:bg-red-retro">
+                    <Link to="/About" className="mx-3">
+                      About
+                    </Link>
+                  </li>
                 </ul>
-            
-            )} 
-          </button>
-          <div className="lg:visible md:invisible sm:invisible md:m-0 sm:m-0 xsm:invisible">
-            <Link to="/" className="mx-3">
-              Home 
-            </Link>
-            <Link to="/Universities" className="mx-3">
-              Universities
-            </Link>
-            <Link to="/Departments" className="mx-3">
-              Departments
-            </Link>
+              )}
+            </button>
+            <div className="lg:visible md:invisible sm:invisible sr-only sm:not-sr-only md:m-0 sm:m-0 ">
+              <Link to="/" className="mx-3">
+                Home
+              </Link>
+              <Link to="/Universities" className="mx-3">
+                Universities
+              </Link>
+              <Link to="/Departments" className="mx-3">
+                Departments
+              </Link>
+            </div>
           </div>
-        </div>
-               
 
-               <div className="w-full h-[82px]  flex items-center justify-end">
-               <button className="mx-5 shadow-md"><img src={langIcon}></img></button>
-            <div className="flex gap-4 h-[51px]  pr-[100px]">
+          <div className="w-full h-[82px] max-[600]:h-96 flex items-center justify-end max-[sm]:justify-center">
+            <button className="mx-5 shadow-md">
+              <img src={langIcon}></img>
+            </button>
+            <div className="flex gap-4 h-[51px]  pr-[100px] max-[600px]:h-96">
               <button
                 onClick={() => setOpenModal(!openModal)}
-                className="border rounded hover:bg-red-retro bg-red-warm text-white-cream px-4 py-2 shadow-md"
+                className="border rounded lg:text-sm md:text-sm max-sm:text-xs sm:text-xs  hover:bg-red-retro bg-red-warm text-white-cream px-4 py-2 shadow-md "
               >
                 Sign In
               </button>
               <button
                 onClick={() => setOpenUpModal(!openUpModal)}
-                className="border rounded  hover:bg-red-retro bg-red-warm	 text-white-cream px-4 py-2 shadow-md"
+                className="border rounded lg:text-sm md:text-sm max-sm:text-xs  sm:text-xs max-[600px]:bg-cyan-800  hover:bg-red-retro bg-red-warm	 text-white-cream px-4 py-2 shadow-md"
               >
                 Sign Up
               </button>
             </div>
           </div>
-      
-
-      </nav>
+        </nav>
       </div>
     </div>
   );
