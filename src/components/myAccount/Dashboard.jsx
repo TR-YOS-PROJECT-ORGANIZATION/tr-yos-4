cdaa12056dbba417d86222e3af785035e4ece5d8
gulfeyzaciftci <gulfeyza333@gmail.com>
2023-07-15 13:28:57 +0200

    responsive düzenlemesi

--- src/components/myAccount/Dashboard.jsx
+++ src/components/myAccount/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Dashboard = () => {
   return (
-    <div className="flex flex-col border h-1/2 justify-center max-w-xs p-6 mt-5 shadow-md rounded-xl sm:px-12 text-dark-900 dark:text-gray-100">
+    <div className="flex flex-col border justify-center p-6 mt-5 shadow-xl rounded-xl h-1/2 xs:w-full sm:px-12 ">
       <img
         src="https://source.unsplash.com/150x150/?portrait?3"
         alt=""
@@ -9,22 +9,24 @@ const Dashboard = () => {
       />
       <div className="space-y-4 text-center divide-y divide-gray-700">
         <div className="my-2 space-y-1">
-          <h2 className="text-xl font-semibold sm:text-2xl">Leroy Jenkins</h2>
-          <p className="px-5 text-xs sm:text-base dark:text-gray-400">
+          <h2 className="text-xl font-semibold sm:text-2xl text-blue-dark ">
+            Leroy Jenkins
+          </h2>
+          <p className="px-5 text-xs sm:text-base text-blue-dark">
             name@gmail.com
           </p>
         </div>
-        <div className="flex justify-center pt-2 space-x-4 align-center">
+        <div className="flex justify-center pt-2 xs:w-full">
           <a
             rel="noopener noreferrer"
             href="#"
-            aria-label="GitHub"
-            className="p-2 rounded-md dark:text-gray-100 hover:dark:text-violet-400"
+            aria-label="Facebook"
+            className="p-2 rounded-md"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              height="1em"
               viewBox="0 0 512 512"
+              className="w-6 h-6 fill-current text-blue-facebook"
             >
               <path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z" />
             </svg>
@@ -33,12 +35,12 @@ const Dashboard = () => {
             rel="noopener noreferrer"
             href="#"
             aria-label="Twitter"
-            className="p-2 rounded-md dark:text-gray-100 hover:dark:text-violet-400"
+            className="p-2 rounded-md"
           >
             <svg
               viewBox="0 0 512 512"
               xmlns="http://www.w3.org/2000/svg"
-              className="w-4 h-4 fill-current"
+              className="w-6 h-6 fill-current text-blue-twitter"
             >
               <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
             </svg>
@@ -46,13 +48,13 @@ const Dashboard = () => {
           <a
             rel="noopener noreferrer"
             href="#"
-            aria-label="Twitter"
-            className="p-2 rounded-md dark:text-gray-100 hover:dark:text-violet-400"
+            aria-label="Google Plus"
+            className="p-2 rounded-md"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              height="1em"
               viewBox="0 0 512 512"
+              className="w-6 h-6 fill-current text-red-google"
             >
               <path d="M256,8C119.1,8,8,119.1,8,256S119.1,504,256,504,504,392.9,504,256,392.9,8,256,8ZM185.3,380a124,124,0,0,1,0-248c31.3,0,60.1,11,83,32.3l-33.6,32.6c-13.2-12.9-31.3-19.1-49.4-19.1-42.9,0-77.2,35.5-77.2,78.1S142.3,334,185.3,334c32.6,0,64.9-19.1,70.1-53.3H185.3V238.1H302.2a109.2,109.2,0,0,1,1.9,20.7c0,70.8-47.5,121.2-118.8,121.2ZM415.5,273.8v35.5H380V273.8H344.5V238.3H380V202.8h35.5v35.5h35.2v35.5Z" />
             </svg>
@@ -60,13 +62,13 @@ const Dashboard = () => {
           <a
             rel="noopener noreferrer"
             href="#"
-            aria-label="Email"
-            className="p-2 rounded-md dark:text-gray-100 hover:dark:text-violet-400"
+            aria-label="LinkedIn"
+            className="p-2 rounded-md"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              height="1em"
               viewBox="0 0 448 512"
+              className="w-6 h-6 fill-current text-blue-linkedin"
             >
               <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
             </svg>
@@ -111,60 +113,56 @@ const Dashboard = () => {
       </div>
       <div className="mt-6">
         <div className="mt-2">
-          <a className=" flex text-xs p-2 font-medium border-2 rounded-md text-gray-base  hover:bg-red-retro hover:text-gray">
-            <div className="pt-1 m-0.5">
+          <a className=" flex p-2 font-medium border-2 rounded-lg bg-gray-light text-gray-base  hover:bg-blue-dark hover:text-white-cream">
+            <div className="pt-1.5 mr-2">
               <svg
-                className="fill-black"
                 xmlns="http://www.w3.org/2000/svg"
-                height="0.9em"
+                height="1em"
                 viewBox="0 0 512 512"
               >
-                <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+                <path d="M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm320 96c0-26.9-16.5-49.9-40-59.3V88c0-13.3-10.7-24-24-24s-24 10.7-24 24V292.7c-23.5 9.5-40 32.5-40 59.3c0 35.3 28.7 64 64 64s64-28.7 64-64zM144 176a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm-16 80a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm288 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64zM400 144a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z" />
               </svg>
             </div>
             <div>Dashboard</div>
           </a>
         </div>
         <div className="mt-2">
-          <a className=" flex text-xs p-2 font-medium border-2 rounded-md text-gray-base  hover:bg-red-retro hover:text-gray">
-            <div className="pt-1 m-0.5">
+          <a className=" flex p-2 font-medium border-2 rounded-lg bg-gray-light text-gray-base  hover:bg-blue-dark hover:text-white-cream">
+            <div className="pt-1 mr-2">
               <svg
-                className="fill-black"
                 xmlns="http://www.w3.org/2000/svg"
-                height="0.9em"
-                viewBox="0 0 512 512"
+                height="1em"
+                viewBox="0 0 448 512"
               >
-                <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+                <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
               </svg>
             </div>
             <div>My Profile</div>
           </a>
         </div>
         <div className="mt-2">
-          <a className=" flex text-xs p-2 font-medium border-2 rounded-md text-gray-base  hover:bg-red-retro hover:text-gray">
-            <div className="pt-1 m-0.5">
+          <a className=" flex p-2 font-medium border-2 rounded-lg bg-gray-light text-gray-base  hover:bg-blue-dark hover:text-white-cream">
+            <div className="pt-1.5 mr-2">
               <svg
-                className="fill-black"
                 xmlns="http://www.w3.org/2000/svg"
-                height="0.9em"
+                height="1em"
                 viewBox="0 0 512 512"
               >
-                <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+                <path d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z" />
               </svg>
             </div>
             <div>Messages</div>
           </a>
         </div>
         <div className="mt-2">
-          <a className=" flex text-xs p-2 font-medium border-2 rounded-md text-gray-base  hover:bg-red-retro hover:text-gray">
-            <div className="pt-1 m-0.5">
+          <a className=" flex p-2 font-medium border-2 rounded-lg bg-gray-light text-gray-base  hover:bg-blue-dark hover:text-white-cream">
+            <div className="pt-1 mr-2">
               <svg
-                className="fill-black"
                 xmlns="http://www.w3.org/2000/svg"
-                height="0.9em"
-                viewBox="0 0 512 512"
+                height="1em"
+                viewBox="0 0 448 512"
               >
-                <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+                <path d="M144 144c0-44.2 35.8-80 80-80c31.9 0 59.4 18.6 72.3 45.7c7.6 16 26.7 22.8 42.6 15.2s22.8-26.7 15.2-42.6C331 33.7 281.5 0 224 0C144.5 0 80 64.5 80 144v48H64c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V256c0-35.3-28.7-64-64-64H144V144z" />
               </svg>
             </div>
             <div>Change Password</div>
