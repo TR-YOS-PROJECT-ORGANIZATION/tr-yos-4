ab66ace427c2f74d82cf918f68e80ed40b5a4bb5
özgebayram <ozgebayram1234@gmail.com>
2023-07-18 23:50:57 +0300

    feat:added departmentDetail responsive

--- src/components/departmentDetail/SubmitReview.jsx
+++ src/components/departmentDetail/SubmitReview.jsx
@@ -7,11 +7,11 @@ const SubmitReview = () => {
     <div className="">
       {/* 1. kısım */}
 
-      <div className="flex mx-5 mt-8">
-        <div className="w-3/2">
+      <div className="flex-none lg:flex  mt-8 w-3/4 m-auto">
+        <div className="">
           <div className="">
-            <div className="w-2/3 p-7 border-2 m-5 rounded-xl flex justify-between shadow-lg">
-              <div className="leading-relaxed ml-5">
+            <div className=" p-7 border-2 m-5 rounded-xl flex-none lg:flex  lg:justify-between shadow-lg">
+              <div className="ml-5 text-left font-bold text-blue-950 text-xl">
                 <h4>Medical Engineering 25% Scholarship</h4>
                 <h4>Medical School</h4>
                 <span className="text-xs text-slate-500">
@@ -22,32 +22,32 @@ const SubmitReview = () => {
                   </a>
                 </span>
               </div>
-              <div className="mr-5 m-auto">
-                <p className="font-bold text-xl">$25000</p>
+              <div className=" font-bold text-left ml-5 ">
+                <p className="text-xl text-blue-700 ">$25000</p>
                 <span className="text-xs text-slate-500">Year</span>
               </div>
             </div>
             {/* other */}
-            <div className="gap-2 border w-2/3 m-5 rounded-xl ">
+            <div className="gap-2 border w-3/2 m-5 rounded-xl ">
               <div className="">
-                <h4 className="ml-6 mt-4 font-bold">Other</h4>
+                <h4 className="ml-6 mt-4 font-bold text-left">Other</h4>
               </div>
 
-              <div className="grid grid-rows-1  p-4">
-                <div className="grid grid-cols-4 text-center bg-[#E5F8F2] border border-1 border-green-300 rounded-xl">
-                  <div className="p-10 ">
+              <div className="p-4 ">
+                <div className="grid-none lg:grid grid-cols-4   md:divide-x text-center bg-[#E5F8F2] border border-green-300 rounded-xl   divide-dotted divide-green-300">
+                  <div className="p-10 md:w-1/4 sm:w-1/2 w-full">
                     <div className="text-xs">Language</div>
                     <div className="font-bold text-[#00A372]">English</div>
                   </div>
-                  <div className="p-10 ">
+                  <div className="p-10 md:w-1/4 sm:w-1/2 w-full">
                     <div className="text-xs">Language</div>
                     <div className="font-bold text-[#00A372]">English</div>
                   </div>
-                  <div className="p-10 ">
+                  <div className="p-10 md:w-1/4 sm:w-1/2 w-full">
                     <div className="text-xs">Language</div>
                     <div className="font-bold text-[#00A372]">English</div>
                   </div>
-                  <div className="p-10 ">
+                  <div className="p-10 md:w-1/4 sm:w-1/2 w-full ">
                     <div className="text-xs">Language</div>
                     <div className="font-bold text-[#00A372]">English</div>
                   </div>
@@ -55,7 +55,7 @@ const SubmitReview = () => {
               </div>
             </div>
             {/* About */}
-            <div className="w-2/3 border ml-5 rounded-xl ">
+            <div className="w-3/2 border m-5 rounded-xl text-left ">
               <div className=" m-5">
                 <h4 className="font-bold">About Department</h4>
               </div>
@@ -130,112 +130,112 @@ const SubmitReview = () => {
                 </p>
               </div>
             </div>
-            {/* <button onClick={() => setIsOpen(!isOpen)} */}
+            
             {/* Dropdown */}
 
-            <div className="border w-2/3 m-5 rounded-xl ">
-              <div className="flex justify-between p-5">
-                <div className="">
-                  <h4 className=" font-bold">Other</h4>
-                </div>
-                <div>
-                  <button onClick={() => setIsOpen(!isOpen)}>
-                    <i class="fa-solid fa-circle-chevron-down"></i>
-                  </button>
-                </div>
+            <div className="border w-3/2 m-5 rounded-xl ">
+            <div className="flex justify-between p-5">
+              <div className="">
+                <h4 className=" font-bold">Other</h4>
               </div>
+              <div>
+                <button onClick={() => setIsOpen(!isOpen)}>
+                  <i class="fa-solid fa-circle-chevron-down"></i>
+                </button>
+              </div>
+            </div>
 
-              {!isOpen && (
-                <section className="text-gray-600 body-font">
-                  <div className="container px-5 py-5 mx-auto">
-                    <div className="flex flex-wrap -m-4 text-center">
-                      <div className="p-4 w-full">
-                        <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                          <svg
-                            width="640px"
-                            height="640px"
-                            viewBox="0 -64 640 640"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="text-indigo-500 w-12 h-12 mb-3 inline-block"
-                          >
-                            <path d="M176 256c44.11 0 80-35.89 80-80s-35.89-80-80-80-80 35.89-80 80 35.89 80 80 80zm352-128H304c-8.84 0-16 7.16-16 16v144H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v352c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-48h512v48c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V240c0-61.86-50.14-112-112-112z" />
-                          </svg>
+            {!isOpen && (
+              <section className="text-gray-600 body-font">
+                <div className="container px-5 py-5 mx-auto">
+                  <div className="flex flex-wrap -m-4 text-center">
+                    <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
+                      <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                        <svg
+                          width="640px"
+                          height="640px"
+                          viewBox="0 -64 640 640"
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="text-indigo-500 w-12 h-12 mb-3 inline-block"
+                        >
+                          <path d="M176 256c44.11 0 80-35.89 80-80s-35.89-80-80-80-80 35.89-80 80 35.89 80 80 80zm352-128H304c-8.84 0-16 7.16-16 16v144H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v352c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-48h512v48c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V240c0-61.86-50.14-112-112-112z" />
+                        </svg>
 
-                          <p className="leading-relaxed">3</p>
-                        </div>
+                        <p className="leading-relaxed">3</p>
                       </div>
-                      <div className="p-4 w-full">
-                        <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                          <svg
-                            height="800px"
-                            width="800px"
-                            version="1.1"
-                            id="_x32_"
-                            xmlns="http://www.w3.org/2000/svg"
-                            xmlns-xlink="http://www.w3.org/1999/xlink"
-                            viewBox="0 0 512 512"
-                            xml-space="preserve"
-                            className=" w-12 h-12 mb-3 inline-block"
-                          >
-                            <style type="text/css"></style>
-                            <g>
-                              <polygon
-                                class="st0"
-                                points="256,381.424 104.628,328.845 0,365.186 256,454.114 512,365.186 407.373,328.845 	"
-                              />
-                              <polygon
-                                class="st0"
-                                points="256,272.235 104.628,219.655 0,255.996 256,344.924 512,255.996 407.373,219.655 	"
-                              />
-                              <polygon
-                                class="st0"
-                                points="512,146.806 256,57.886 0,146.806 256,235.734 	"
-                              />
-                            </g>
-                          </svg>
+                    </div>
+                    <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
+                      <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                        <svg
+                          height="800px"
+                          width="800px"
+                          version="1.1"
+                          id="_x32_"
+                          xmlns="http://www.w3.org/2000/svg"
+                          xmlns-xlink="http://www.w3.org/1999/xlink"
+                          viewBox="0 0 512 512"
+                          xml-space="preserve"
+                          className=" w-12 h-12 mb-3 inline-block"
+                        >
+                          <style type="text/css"></style>
+                          <g>
+                            <polygon
+                              class="st0"
+                              points="256,381.424 104.628,328.845 0,365.186 256,454.114 512,365.186 407.373,328.845 	"
+                            />
+                            <polygon
+                              class="st0"
+                              points="256,272.235 104.628,219.655 0,255.996 256,344.924 512,255.996 407.373,219.655 	"
+                            />
+                            <polygon
+                              class="st0"
+                              points="512,146.806 256,57.886 0,146.806 256,235.734 	"
+                            />
+                          </g>
+                        </svg>
 
-                          <p className="leading-relaxed">4,240</p>
-                        </div>
+                        <p className="leading-relaxed">4,240</p>
                       </div>
-                      <div className="p-4 w-full">
-                        <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                          <svg
-                            width="640px"
-                            height="640px"
-                            viewBox="0 -64 640 640"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className=" w-12 h-12 mb-3 inline-block"
-                          >
-                            <path d="M504 352H136.4c-4.4 0-8 3.6-8 8l-.1 48c0 4.4 3.6 8 8 8H504c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8zm0 96H136.1c-4.4 0-8 3.6-8 8l-.1 48c0 4.4 3.6 8 8 8h368c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8zm0-192H136.6c-4.4 0-8 3.6-8 8l-.1 48c0 4.4 3.6 8 8 8H504c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8zm106.5-139L338.4 3.7a48.15 48.15 0 0 0-36.9 0L29.5 117C11.7 124.5 0 141.9 0 161.3V504c0 4.4 3.6 8 8 8h80c4.4 0 8-3.6 8-8V256c0-17.6 14.6-32 32.6-32h382.8c18 0 32.6 14.4 32.6 32v248c0 4.4 3.6 8 8 8h80c4.4 0 8-3.6 8-8V161.3c0-19.4-11.7-36.8-29.5-44.3z" />
-                          </svg>
+                    </div>
+                    <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
+                      <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                        <svg
+                          width="640px"
+                          height="640px"
+                          viewBox="0 -64 640 640"
+                          xmlns="http://www.w3.org/2000/svg"
+                          className=" w-12 h-12 mb-3 inline-block"
+                        >
+                          <path d="M504 352H136.4c-4.4 0-8 3.6-8 8l-.1 48c0 4.4 3.6 8 8 8H504c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8zm0 96H136.1c-4.4 0-8 3.6-8 8l-.1 48c0 4.4 3.6 8 8 8h368c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8zm0-192H136.6c-4.4 0-8 3.6-8 8l-.1 48c0 4.4 3.6 8 8 8H504c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8zm106.5-139L338.4 3.7a48.15 48.15 0 0 0-36.9 0L29.5 117C11.7 124.5 0 141.9 0 161.3V504c0 4.4 3.6 8 8 8h80c4.4 0 8-3.6 8-8V256c0-17.6 14.6-32 32.6-32h382.8c18 0 32.6 14.4 32.6 32v248c0 4.4 3.6 8 8 8h80c4.4 0 8-3.6 8-8V161.3c0-19.4-11.7-36.8-29.5-44.3z" />
+                        </svg>
 
-                          <p className="leading-relaxed">1</p>
-                        </div>
+                        <p className="leading-relaxed">1</p>
                       </div>
-                      <div className="p-4 w-full">
-                        <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                          {/* className="text-indigo-500 w-12 h-12 mb-3 inline-block" */}
-                          <svg
-                            width="640px"
-                            height="640px"
-                            viewBox="0 -64 640 640"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="text-indigo-500 w-12 h-12 mb-3 inline-block"
-                          >
-                            <path d="M176 256c44.11 0 80-35.89 80-80s-35.89-80-80-80-80 35.89-80 80 35.89 80 80 80zm352-128H304c-8.84 0-16 7.16-16 16v144H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v352c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-48h512v48c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V240c0-61.86-50.14-112-112-112z" />
-                          </svg>
+                    </div>
+                    <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
+                      <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                        {/* className="text-indigo-500 w-12 h-12 mb-3 inline-block" */}
+                        <svg
+                          width="640px"
+                          height="640px"
+                          viewBox="0 -64 640 640"
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="text-indigo-500 w-12 h-12 mb-3 inline-block"
+                        >
+                          <path d="M176 256c44.11 0 80-35.89 80-80s-35.89-80-80-80-80 35.89-80 80 35.89 80 80 80zm352-128H304c-8.84 0-16 7.16-16 16v144H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v352c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-48h512v48c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V240c0-61.86-50.14-112-112-112z" />
+                        </svg>
 
-                          <p className="leading-relaxed">Active</p>
-                        </div>
+                        <p className="leading-relaxed">Active</p>
                       </div>
                     </div>
                   </div>
-                </section>
-              )}
-            </div>
+                </div>
+              </section>
+            )}
+          </div>
 
             {/* submit review */}
-            <div className="border rounded-xl m-5 w-2/3 grid grid-col-2">
+            <div className="border rounded-xl m-5 w-3/2 grid grid-col-2">
               <div className="m-5 col-span-2">
                 <h2 className="text-2xl font-bold">Submit Review</h2>
                 <div className="flex gap-3 mt-3 col-span-2">
