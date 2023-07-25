c7f16c8410bf6c44f2ffe11b5c0d55a6b9a95d14
Zehra Celik <escev123@gmail.com>
2023-07-25 20:46:36 +0300

    edited app router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -21,12 +21,19 @@ const AppRouter = () => {
                 <Route path='/' element={<HomePage/>} />
                 <Route path='Departments' element={<Departments />} />
                 <Route path='Universities' element={<UniversitiesPage/>} />
-                <Route path="myProfile" element={<MyProfile/>} />
-                <Route path="favourites" element={<Favourites/>} />
+                
                 <Route path="/departmentDetail/:code" element={<DepartmentDetail />} />
 
-                <Route path="/compareList" element={<PrivateRouter />}>
+                <Route path="/compare" element={<PrivateRouter />}>
                 <Route path="" element={<Compare/>} />
+                </Route>
+
+                <Route path="myProfile" element={<PrivateRouter />}>
+                <Route path="" element={<MyProfile/>} />
+                </Route>
+
+                <Route path="favourites" element={<PrivateRouter />}>
+                <Route path="" element={<Favourites/>} />
                 </Route>
 
                 
