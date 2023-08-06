e43f355332eeeca4f5567f306d8b1d84896be132
özgebayram <ozgebayram1234@gmail.com>
2023-08-06 22:04:14 +0300

    signUp

--- src/components/modals/SignUpModal.jsx
+++ src/components/modals/SignUpModal.jsx
@@ -1,7 +1,5 @@
-/* eslint-disable react/prop-types */
 import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-
 import { Formik, Field, Form } from "formik";
 import * as yup from "yup";
 import useAuthCall from "../../hooks/useAuthCall";
@@ -9,17 +7,14 @@ import { useSelector } from "react-redux";
 import { ErrorMessage } from "formik";
 import close from '../../images/close.png'
 import { useTranslation } from "react-i18next";
-
 export const registerSchema = yup.object().shape({
   name: yup
     .string()
     .max(10, "name must have less than 10 chars")
     .required("Please enter an username")
     .nullable(),
-
   email: yup.string()
             .required(),
-
   password1: yup
     .string()
     .required("Password must have min 8 chars,a number, a lowercase, an uppercase, a special char")
@@ -30,15 +25,10 @@ export const registerSchema = yup.object().shape({
     .matches(/[A-Z]+/, "Password must have an uppercase")
     .matches(/[!,?{}><%&$#£+-.]+/, " Password must have a special char"),
 });
-
-
 const SignUpModal = (props) => {
   const { currentUser } = useSelector((state) => state?.auth);
   const { register } = useAuthCall();
   const {t} = useTranslation();
-
-
-
   // const copyDeployerWallet = () => {
   //   navigator.clipboard.writeText("0x49AE63056b3A0Be0B166813eE687309Ab653c07c");
   //   // toast.success("Address copied");
@@ -62,7 +52,6 @@ const SignUpModal = (props) => {
           >
             <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
           </Transition.Child>
-
           {/* This element is to trick the browser into centering the modal contents. */}
           {/* <span
             className="hidden sm:inline-block sm:align-middle sm:h-screen"
@@ -80,14 +69,12 @@ const SignUpModal = (props) => {
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
             <div
-              className="text-white-500 w-[500px] min-h-[450px] border-2 border-spacing-3 
+              className="text-white-500 w-[500px] min-h-[450px] border-2 border-spacing-3
             border-red-950 outline outline-offset-4  p-5 rounded-lg overflow-hidden transform transition-all bg-white-500 relative"
             >
             <button onClick={()=>props.setOpenUp(false)}>
             <img src={close} className=" absolute right-5 top-5 hover:opacity-50"></img>
-
             </button>
-
               <p className="text-black font-bold text-lg">{t("Sign Up")}</p>
               <div className="m-5 rounded items-start text-start">
                 <Formik
@@ -112,13 +99,10 @@ const SignUpModal = (props) => {
                     touched,
                     handleBlur,
                     isSubmitting,
-                   
-                    
                   }) => (
                     <Form>
                       <div className="flex flex-col text-black ">
                         <label className="mt-2 font-bold" htmlFor="name">{t("Username")}</label>
-
                         <Field
                           className="border border-green-dark my-1 p-1 rounded "
                           id="name"
@@ -128,15 +112,11 @@ const SignUpModal = (props) => {
                           onChange={handleChange}
                           value={values.name}
                         />
-                         {touched.name && errors.name ? 
+                         {touched.name && errors.name ?
                          <div className="text-red-retro mb-2 text-sm "> {errors.name}</div>
-                                   
                                      : null}
-
                         {/* <ErrorMessage name="name" /> */}
-
                         <label className="mt-2 font-bold" htmlFor="email">{t("Email")}</label>
-
                         <Field
                           className="border border-green-dark my-1 p-1 rounded"
                           id="email"
@@ -149,11 +129,8 @@ const SignUpModal = (props) => {
                           variant="outlined"
                         />
                           {touched.email && errors.email ?  <div className="text-red-retro mb-2 text-sm "> {errors.email}</div>
-                                   
                                    : null}
-
                         <label className="mt-2 font-bold" htmlFor="password">{t("Password")}</label>
-
                         <Field
                           className="border border-green-dark my-1 p-1 rounded"
                           id="password1"
@@ -165,9 +142,7 @@ const SignUpModal = (props) => {
                           value={values.password1}
                         />
                          {touched.password1 && errors.password1 ? <div className="text-red-retro mb-2 text-sm "> {errors.password1}</div>
-                                   
                                    : null}
-
                         <Field
                           className="border border-green-dark my-3 p-1 rounded"
                           id="password2"
@@ -178,8 +153,6 @@ const SignUpModal = (props) => {
                           onChange={handleChange}
                           value={values.password2}
                         />
-                      
-
                         <button
                           className="bg-red-warm mx-auto rounded py-3 mt-5 text-white-500 w-48"
                           type="submit"
@@ -199,5 +172,4 @@ const SignUpModal = (props) => {
     </Transition.Root>
   );
 };
-
 export default SignUpModal;
