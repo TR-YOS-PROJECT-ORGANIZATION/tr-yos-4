8d507179fced9ba9a199fe3b472c3627b412eee6
MEHMETTOKGOZ <42mt06@gmail.com>
2023-08-06 21:52:29 +0300

    useCardCall last

--- src/hooks/useCardCalls.jsx
+++ src/hooks/useCardCalls.jsx
@@ -9,13 +9,11 @@ import {
 } from "../features/cardSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { toastErrorNotify } from "../helper/ToastNotify";
-
 const useCardCalls = () => {
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.auth);
   const { favouriteList, compareList } = useSelector((state) => state.card);
-  const currentUserId = currentUser?.userID
-
+  const currentUserId = currentUser?.user.userId
   // Add or Remove to Compare
   const moveToSelectedDepartments = async (id) => {
     const departmentId = id;
@@ -23,10 +21,8 @@ const useCardCalls = () => {
      const res = await axios.get(
         `https://tr-yös.com/api/v1/users/addcompare.php?user_id=${currentUserId}&id=${departmentId}&token=KE4ekFg1YPngkIbjMP/5JdBtisNVE076kWUW7TPz8iGaHT8te/i2nrAycAGnwAL5ZRitK5Rb4VwDp6JEfab5b0d5dfc31a7d39edf5370b8a067a`
       );
-      console.log("move c",res);
       const newDeps = compareList?.departments ? compareList?.departments?.concat([res.data.id]) : [res?.data.id];
       dispatch(getCompareListSuccess({ departments: newDeps }));
-
     } catch (error) {
       console.log(error);
     }
@@ -37,11 +33,9 @@ const useCardCalls = () => {
       const res =  await axios.get(
         `https://tr-yös.com/api/v1/users/deletecompare.php?user_id=${currentUserId}&id=${departmentId}&token=KE4ekFg1YPngkIbjMP/5JdBtisNVE076kWUW7TPz8iGaHT8te/i2nrAycAGnwAL5ZRitK5Rb4VwDp6JEfab5b0d5dfc31a7d39edf5370b8a067a`
       );
-      console.log("rem c",res);
       const newDeps = compareList?.departments?.filter(
         (f) => f !== res.data.id
       );
-
       dispatch(getCompareListSuccess({ departments: newDeps }));
     } catch (error) {
       console.log(error);
@@ -54,13 +48,8 @@ const useCardCalls = () => {
       const res = await axios.get(
         `https://tr-yös.com/api/v1/users/addfavorite.php?id=${departmentId}&user_id=${currentUserId}&token=KE4ekFg1YPngkIbjMP/5JdBtisNVE076kWUW7TPz8iGaHT8te/i2nrAycAGnwAL5ZRitK5Rb4VwDp6JEfab5b0d5dfc31a7d39edf5370b8a067a`
       );
-      console.log("move f",res);
-
       const newDeps = favouriteList?.departments ? favouriteList?.departments?.concat([res.data.id])
       :[ res.data.id];
-      console.log("fl",favouriteList);
-      console.log("newdeps",newDeps);
-
       dispatch(getFavouriteListSuccess({ departments: newDeps }));
     } catch (error) {
       console.log(error);
@@ -69,17 +58,13 @@ const useCardCalls = () => {
   };
   const removeFromFavourites = async (id) => {
     const departmentId = id;
-
     try {
       const res = await axios.get(
         `https://tr-yös.com/api/v1/users/deletefavorite.php?id=${departmentId}&user_id=${currentUserId}&token=KE4ekFg1YPngkIbjMP/5JdBtisNVE076kWUW7TPz8iGaHT8te/i2nrAycAGnwAL5ZRitK5Rb4VwDp6JEfab5b0d5dfc31a7d39edf5370b8a067a`
       );
-      console.log("rem f",res);
-
       const newDeps = favouriteList?.departments?.filter(
         (f) => f !== res.data.id
       );
-
       dispatch(getFavouriteListSuccess({ departments: newDeps }));
     } catch (error) {
       console.log(error);
@@ -90,7 +75,7 @@ const useCardCalls = () => {
     dispatch(fetchStart());
     try {
       const { data } = await axios.get(
-        `https://tr-yös.com/api/v1/users/allcompares.php?user_id=${currentUser?.userID}&token=KE4ekFg1YPngkIbjMP/5JdBtisNVE076kWUW7TPz8iGaHT8te/i2nrAycAGnwAL5ZRitK5Rb4VwDp6JEfab5b0d5dfc31a7d39edf5370b8a067a`
+        `https://tr-yös.com/api/v1/users/allcompares.php?user_id=${currentUser?.user.userId}&token=KE4ekFg1YPngkIbjMP/5JdBtisNVE076kWUW7TPz8iGaHT8te/i2nrAycAGnwAL5ZRitK5Rb4VwDp6JEfab5b0d5dfc31a7d39edf5370b8a067a`
       );
       dispatch(getCompareListSuccess(data));
     } catch (error) {
@@ -104,7 +89,6 @@ const useCardCalls = () => {
         // no record found hatası döndüğünde; favouriteList.department = [] yapıyoruz.
         dispatch(getCompareListSuccess({ departments: [] }));
       }
-
       dispatch(fetchFail());
     }
   };
@@ -112,7 +96,7 @@ const useCardCalls = () => {
     dispatch(fetchStart());
     try {
       const { data } = await axios.get(
-        `https://tr-yös.com/api/v1/users/allfavorites.php?user_id=${currentUser.userID}&token=KE4ekFg1YPngkIbjMP/5JdBtisNVE076kWUW7TPz8iGaHT8te/i2nrAycAGnwAL5ZRitK5Rb4VwDp6JEfab5b0d5dfc31a7d39edf5370b8a067a`
+        `https://tr-yös.com/api/v1/users/allfavorites.php?user_id=${currentUser.user.userId}&token=KE4ekFg1YPngkIbjMP/5JdBtisNVE076kWUW7TPz8iGaHT8te/i2nrAycAGnwAL5ZRitK5Rb4VwDp6JEfab5b0d5dfc31a7d39edf5370b8a067a`
       );
       dispatch(getFavouriteListSuccess(data));
     } catch (error) {
